c2a0822630ebfb85e789d978d46879188aa755cf
T.E Attigah <>
2017-01-13 01:02:38 +0000

    Modified digital signature implementation and its verification

---
+++ src/main/java/com/tbc/hello/myfirstspringapi/Application.java
@@ -0,0 +1,30 @@
+package com.tbc.hello.myfirstspringapi;
+
+import java.util.Date;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author Theodore Elikem Attigah
+ */
+@SpringBootApplication
+@RestController
+public class Application {
+    
+    @RequestMapping("/hello")
+    public String sayHello() {
+        return "Hello World!";
+    }
+    
+    @RequestMapping("/")
+    public String sayTime() {
+        return new Date().toString();
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
